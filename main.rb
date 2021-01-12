84d305c983e0c0a8f2318dc82aa2bdb65c1bdad8
MoretS <s.moret@ecole-ipssi.net>
2021-01-12 10:34:54 +0100

    Add comments

--- main.rb
+++ main.rb
@@ -4,17 +4,19 @@ require 'net/http'
 require 'uri'
 require 'json'
 
-BACKUP_RETENTION = ENV.fetch("BACKUP_RETENTION", 0)
+# Number of backup we want to keep for each instance
+BACKUP_RETENTION = ENV.fetch("BACKUP_RETENTION", 1)
 
 # Listing instances
 instances = `scw instance server list`.split("\n")
                                       .map { |row| row.split(/\s{2,}/o)}
                                       .drop(1)
 
-data_output = {}
+# Init hash with listed instances and data for each instance
+instances_data_output = {}
 
 instances.each do |row| 
-    data_output[row[1]] = {
+    instances_data_output[row[1]] = {
                 "ID" => row[0], 
                 "NAME" => row[1], 
                 "TYPE" => row[2], 
@@ -36,29 +38,36 @@ instances.each do |row|
             }
 end
 
-# Post request to create a backup
-data_output.each do |instance, instance_data|
+# Post request to create a backup for each instance
+instances_data_output.each do |instance, instance_data|
+    # Init vars
     server_id = instance_data["ID"]
     timestamp = Time.now.utc.strftime("%Y-%m-%d_%H-%M")
     server_name = instance_data["NAME"]
     backup_name = "backup_image_#{server_name}_#{timestamp}"
 
+    # Header and body content for the request
     header = { 'Content-Type' => 'application/json', 'X-Auth-Token' => ENV["SECRET_KEY"] }
     body = { 'action': 'backup', 'name': backup_name }
 
+    # Building request
     uri = URI.parse("https://api.scaleway.com/instance/v1/zones/fr-par-1/servers/#{server_id}/action")
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
     req = Net::HTTP::Post.new(uri.request_uri, header)
     req.body = body.to_json
+
+    # Execute request and send response 
     response = http.request(req)
 end
 # End of post request
 
+# Listing images
 images = `scw instance image list`.split("\n")
                                   .map { |row| row.split(/\s{2,}/o)}
                                   .drop(1)
 
+# Init hash with listed images and data for each image
 images_data_output = {}
 
 images.each do |row| 
@@ -78,25 +87,30 @@ images.each do |row|
                 "MODIFICATION DATE" => row[12]
             }
 end
-        
-data_output.dup.each do |instance_name, instance_data|
+ 
+# Add existing images for each instance in listed instances hash
+instances_data_output.dup.each do |instance_name, instance_data|
     instance_data["IMAGES"] = images_data_output.select{ |image_name, image_data| instance_data["ID"] == image_data["SERVER ID"]}
 end
 
+# Listing snapshots
 snapshots = `scw instance snapshot list`.split("\n")
                                         .drop(1)
                                         .map{ |row| row.split(/\s{2,}/o)}
                                         .map{ |row| {"ID" => row[0], "NAME" => row[1]}}
 
-data_output.each do |instance_name, instance_data|
+# List all images for each instance and exclude the last backup images according to the backup retention number
+instances_data_output.each do |instance_name, instance_data|
     images_to_destroy = instance_data["IMAGES"].select{ |image_name, image_data| image_data["NAME"].start_with?("backup_image") }
     images_to_destroy = images_to_destroy.drop(BACKUP_RETENTION)
 
+    # For each image in images_to_destroy array, run scw command to delete the image
     images_to_destroy.each do |image_name, image_data| 
         image_id = image_data["ID"]
         system("scw instance image delete #{image_id}")
-        snapshots_to_destroy = snapshots.select { |snapshot| snapshot["NAME"].start_with?(image_data["NAME"]) }
 
+        # Also delete each snapshot linked to destroyed image
+        snapshots_to_destroy = snapshots.select { |snapshot| snapshot["NAME"].start_with?(image_data["NAME"]) }
         snapshots_to_destroy.each do |snapshot|
             system("scw instance snapshot delete #{snapshot["ID"]}")
         end
